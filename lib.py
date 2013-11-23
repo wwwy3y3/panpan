9872f73f48c6c8622b56c9509a0a1dbb9321be2f
william <wwwy3y3@gmail.com>
2013-11-24 05:19:49 +0800

    update

--- lib.py
+++ lib.py
@@ -23,15 +23,75 @@ class Machine(object):
 	"""the machine"""
 	def __init__(self, capacity):
 		self.times= 0
+		self.day= 0 # start from day0
 		self.orders= []
+		self.leftOrders= [] # order not finish
 		self.capacity= capacity
+		self.capacityLeft= capacity
 
-	def take(self, item, key):
+	def take(self, item, order):
+		if self.capacityLeft <= 0:
+			raise error("capacity over")
+
 		# item= {a: 2}
-		item["key"]= key
-		self.orders.append(item)
+		item["key"]= order.key
+		
+		# add item in orders
+		# if not init a list yet, init one
+		if len(self.orders) <= self.day:
+			self.orders.append([])
+
+		# if over capacity
+		for key,val in item.iteritems():
+			if self.capacityLeft - val < 0:
+				item[key] -= self.capacity
+				self.leftOrders.append(item) # push to tomorrow's work
+
+		# append
+		self.orders[self.day].append(item)
+
+		# add order time in machine
+		# in order to check how many orders it done
 		for val in item.values():
 			self.times += val
+			order.times -= val # order times need to be cut
+
+
+	def shut(self):
+		self.day += 1
+
+	def start(self):
+		self.times= 0
+		self.capacityLeft= self.capacity
+		# get job done yesterday
+		if len(self.leftOrders) > 0:
+			self.take(self.leftOrders.pop())
 
 	def __repr__(self):
-		return repr(self.orders)
+		string= 'machine orders\n'
+		i=0
+		for dayOrders in self.orders:
+			i +=1
+			string += 'day'+ str(i) + '\n'
+			for order in dayOrders:
+				orderStr= 'order key: {0}, item: {1}, time: {2}\n'
+				for key,val in order.iteritems():
+					if key == 'key':
+						orderKey= str(val)
+					else:
+						orderItem= key
+						orderVal= str(val)
+				string += orderStr.format(orderKey, orderItem, orderVal)
+		return string
+
+def orderLeft(orders):
+	for order in orders:
+		if len(order.items) > 0:
+			return True
+	return False
+
+def capacityLeft(machines):
+	for machine in machines:
+		if machine.capacityLeft > 0:
+			return True
+	return False
--- schedule.py
+++ schedule.py
@@ -29,13 +29,35 @@ for item in data["orders"]:
 # orders
 orders= [ Order(**order) for order in ori_orders ]
 # sort by times/deadline
-orders= sorted(orders, key=lambda order: order.coef, reverse=True)
+# orders= sorted(orders, key=lambda order: order.coef, reverse=True)
 # pprint(orders)
 # schedule
-for order in orders:
-	for item in order.items:
-		minM= min(machines, key= lambda machine: machine.times)
-		minM.take(item, order.key)
+# while(orders not all done, orders legnth > 0)
+#	sort orders remains
+#	pop the items in orders
+#	min machine take it
+#	if capacity is over
+#		leave it next day
+#	machine day++, cut the times pass in orders
+
+while orderLeft(orders):
+	orders= sorted(orders, key=lambda order: order.times/order.deadline, reverse= True)
+	for machine in machines:
+		machine.start()
+
+	for order in orders:
+		for item in order.items:
+			minM= min(machines, key= lambda machine: machine.times)
+			minM.take(item, order)
+			order.items.remove(item)
+
+	# end of day
+
+	for order in orders:
+		order.deadline -= 1
+
+	for machine in machines:
+		machine.shut()
 
 pprint(machines)
 # check if okay
